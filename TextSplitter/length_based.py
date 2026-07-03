1d8a51814c541a5ba68d25477cf5c9017ee64f0f
cv maker <adityagupta250406@gmail.com>
2026-07-04 00:37:18 +0530

    Added length based chunking

--- TextSplitter/length_based.py
+++ TextSplitter/length_based.py
@@ -1,3 +1,14 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
 
-print("Import successful!")
+
+loader = PyPDFLoader('Steve_Jobs.pdf')
+docs = loader.load()
+query = docs[2].page_content
+
+splitter = CharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=20
+    )
+chunks = splitter.split_text(query)
+print(chunks)
